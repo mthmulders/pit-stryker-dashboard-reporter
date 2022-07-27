460a3fa80c7a83eab3d39130b850e7a9c117a298
Maarten Mulders <m.th.mulders@xs4all.nl>
2022-07-27 20:51:53 +0200

    feat: Improve test coverage

--- src/test/java/it/mulders/stryker/pitreporter/ReaderInputStreamTest.java
+++ src/test/java/it/mulders/stryker/pitreporter/ReaderInputStreamTest.java
@@ -1,11 +1,14 @@
 package it.mulders.stryker.pitreporter;
 
 import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.StringReader;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class ReaderInputStreamTest implements WithAssertions {
     @Test
     void should_read_until_end() throws IOException {
---
+++ src/test/java/it/mulders/stryker/pitreporter/StrykerDashboardMutationResultListenerFactoryTest.java
@@ -0,0 +1,45 @@
+package it.mulders.stryker.pitreporter;
+
+import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.pitest.coverage.CoverageDatabase;
+import org.pitest.coverage.NoCoverage;
+import org.pitest.mutationtest.ListenerArguments;
+import org.pitest.mutationtest.MutationResultListenerFactory;
+import org.pitest.mutationtest.SourceLocator;
+
+import java.io.Reader;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.Properties;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class StrykerDashboardMutationResultListenerFactoryTest implements WithAssertions {
+    private final MutationResultListenerFactory factory = new StrykerDashboardMutationResultListenerFactory();
+
+    @Test
+    void should_have_name() {
+        assertThat(factory.name()).isEqualTo("stryker-dashboard");
+    }
+
+    @Test
+    void should_have_description() {
+        assertThat(factory.description()).isEqualTo("Stryker Dashboard reporter for PIT");
+    }
+
+    @Test
+    void should_create_listener() {
+        // arrange
+        var coverage = new NoCoverage();
+        var args = new ListenerArguments(null, coverage, null, null, System.currentTimeMillis(), false, null);
+
+        // act
+        var listener = factory.getListener(new Properties(), args);
+
+        // assert
+        assertThat(listener).isInstanceOf(StrykerDashboardMutationResultListener.class)
+                .hasFieldOrPropertyWithValue("coverage", coverage);
+    }
+}
